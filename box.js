09df621d69965351bd36d087b82f063b81c0f1ea
ZeneS13 <sangoi.zene@gmail.com>
2020-06-16 15:48:09 +0530

    modified

--- box.js
+++ box.js
@@ -9,11 +9,21 @@ class Box {
     this.width = width;
     this.height = height;
     this.image = loadImage("pics/virus.png");
+    this.Visiblity = 255;
     World.add(world,this.body);
   }
   display(){
     var pos =this.body.position;
     var angle=this.body.angle;
+   if(this.body.speed > 3){
+    World.remove(world, this.body);
+    push();
+    this.Visiblity = this.Visiblity - 5;
+    tint(255,this.Visiblity);
+    image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+    pop();
+     }
+     
 
     push();
     translate(pos.x,pos.y);
